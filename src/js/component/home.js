031a3e19a00dee0d05c421fca0582112d859c00d
naglenda31 <naglenda31@gmail.com>
2021-03-27 15:42:23 +0000

    component updated and styles applied

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,6 +1,32 @@
 import React, { useState } from "react";
 
 //create your first component
-export function Home() {
-	return <div className="text-center mt-5"></div>;
+export function TrafficLight() {
+	const [color, setColor] = useState();
+	return (
+		<div className="mt-5">
+			<div className="lineHandle"></div>
+			<div className="container">
+				<div
+					className={
+						color === "red" ? "light red selected" : "light red"
+					}
+					onClick={() => setColor("red")}></div>
+				<div
+					className={
+						color === "yellow"
+							? "light yellow selected"
+							: "light yellow"
+					}
+					onClick={() => setColor("yellow")}></div>
+				<div
+					className={
+						color === "green"
+							? "light green selected"
+							: "light green"
+					}
+					onClick={() => setColor("green")}></div>
+			</div>
+		</div>
+	);
 }
