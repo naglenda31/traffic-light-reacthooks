c38a43f5667882ac5d3d32c4c6626b55d29331f3
naglenda31 <naglenda31@gmail.com>
2021-03-31 03:06:27 +0000

    updated css deleted grid layout

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,55 +1,50 @@
 import React, { useState } from "react";
 
-//create your first component
 export function TrafficLight() {
 	const [color, setColor] = useState();
 	return (
 		<div className="wrapper">
-			<div className="lineTop"></div>
+			<div className="trafficLight">
+				<div className="lineHandle"></div>
+				<div className="container">
+					<div
+						className={
+							color === "red" ? "light red selected" : "light red"
+						}
+						onClick={() => setColor("red")}></div>
+					<div
+						className={
+							color === "yellow"
+								? "light yellow selected"
+								: "light yellow"
+						}
+						onClick={() => setColor("yellow")}></div>
+					<div
+						className={
+							color === "green"
+								? "light green selected"
+								: "light green"
+						}
+						onClick={() => setColor("green")}></div>
+				</div>
+			</div>
 
-			<div className="row">
-				<div className="col p-0">
-					<div className="lineHandle"></div>
-					<div className="container">
-						<div
+			<div className="pedestrian-light">
+				<div className="pedestrian-sign">
+					<div>
+						<i
 							className={
 								color === "red"
-									? "light red selected"
-									: "light red"
+									? "fas fa-walking walk"
+									: color === "green"
+									? "fas fa-hand-paper stop"
+									: " "
 							}
-							onClick={() => setColor("red")}></div>
-						<div
-							className={
-								color === "yellow"
-									? "light yellow selected"
-									: "light yellow"
-							}
-							onClick={() => setColor("yellow")}></div>
-						<div
-							className={
-								color === "green"
-									? "light green selected"
-									: "light green"
-							}
-							onClick={() => setColor("green")}></div>
+						/>
 					</div>
 				</div>
-
-				<div className="col align-self-end p-0">
-					<div className="pedestrian-light">
-						<div>
-							<i
-								className={
-									color === "red"
-										? "fas fa-walking walk"
-										: color === "green"
-										? "fas fa-hand-paper stop"
-										: " "
-								}
-							/>
-						</div>
-					</div>
-				</div>
+				<div className="pedestrian-light-pole"></div>
+				<div className="pole-base"></div>
 			</div>
 		</div>
 	);
